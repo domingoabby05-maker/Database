1bfaa03dc9d297d3e23a052e91791d1d955f18fc
domingoabby05-maker <domingoabby05@gmail.com>
2026-05-11 20:17:34 +0700

    tables

---
+++ tables.sql
@@ -0,0 +1,20 @@
+select * from patients;
+select * from next_of_kin;
+select * from local_doctors;
+select * from patient_referrals;
+
+select * from staff;
+select * from staff_contracts;
+select * from work_experience;
+select * from qualifications;
+select * from ward_staff;
+
+select * from wards;
+select * from beds;
+select * from admissions;
+
+select * from appointments;
+select * from outpatients;
+select * from drugs;
+select * from patient_medication;
+
